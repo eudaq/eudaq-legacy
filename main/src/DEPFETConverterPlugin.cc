d57166071d5190de1932faa8e8e2ae67fc4da5be
Emlyn Corrin <emlyn.corrin@gmail.com>
2009-08-19 11:51:03 +0000

    Updated DEPFETConverterPlugin from Julia, with my fixes for new StandardPlane interface

--- main/src/DEPFETConverterPlugin.cc
+++ main/src/DEPFETConverterPlugin.cc
@@ -9,7 +9,19 @@
 #  include "IMPL/LCEventImpl.h"
 #  include "IMPL/TrackerRawDataImpl.h"
 #  include "IMPL/LCCollectionVec.h"
+#  include "UTIL/CellIDEncoder.h"
 #  include "lcio.h"
+#endif
+
+#if USE_EUTELESCOPE
+#  include "EUTELESCOPE.h"
+#  include "EUTelRunHeaderImpl.h"
+#  include "EUTelDEPFETDetector.h"
+#  include "EUTelSetupDescription.h"
+#  include "EUTelEventImpl.h"
+#  include "EUTelSparseDataImpl.h"
+#  include "EUTelSimpleSparsePixel.h"
+using eutelescope::EUTELESCOPE;
 #endif
 
 #include <iostream>
@@ -20,18 +32,24 @@ namespace eudaq {
 
   class DEPFETConverterBase {
   public:
+
+#if USE_LCIO && USE_EUTELESCOPE
+    void ConvertLCIOHeader(lcio::LCRunHeader & header, eudaq::Event const & bore, eudaq::Configuration const & conf) const;
+    bool ConvertLCIO(lcio::LCEvent & lcioEvent, const Event & eudaqEvent) const;
+#endif
+
     StandardPlane ConvertPlane(const std::vector<unsigned char> & data, unsigned id) const {
       StandardPlane plane(id, "DEPFET", "?");
       //     plane.m_tluevent = getlittleendian<unsigned>(&data[4]);
       plane.m_tluevent = getlittleendian<unsigned>(&data[4]);
       int Startgate=(getlittleendian<unsigned>(&data[8])>>10) & 0x7f;
       int DevType=(getlittleendian<unsigned>(&data[0])>>28) & 0xf;
-      printf("TLU=%d Startgate=%d DevType=0x%x \n",plane.m_tluevent,Startgate,DevType);
+      //  printf("TLU=%d Startgate=%d DevType=0x%x \n",plane.m_tluevent,Startgate,DevType);
       int ix,iy,i,j;
       if(DevType==0x3) {
         plane.SetSizeRaw(64, 256);
 
-        for (int gate = 0; gate < plane.m_ysize/2; gate ++)  {      // Pixel sind uebereinander angeordnet
+        for (unsigned gate = 0; gate < plane.m_ysize/2; gate++)  {      // Pixel sind uebereinander angeordnet
           int readout_gate;
           //   if (Direction == false) // default
           readout_gate = (Startgate + gate)%(plane.m_ysize/2);
@@ -39,7 +57,7 @@ namespace eudaq {
           //readout_gate = (startgate[ii] + 64 - gate)%64;
 
           int odderon;
-          for (int col = 0; col < plane.m_xsize/2; col += 2) {
+          for (unsigned col = 0; col < plane.m_xsize/2; col += 2) {
             odderon = readout_gate %2;      //  = 0 for even, = 1 for odd
 
             // eight cases:       -- new! Considers Matrix->Curo Bondpad mismatch! --
@@ -54,8 +72,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4);   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -76,8 +94,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 1 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -91,8 +109,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 2 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -106,8 +124,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 3 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -122,8 +140,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 4 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
             // 6. D, ramzelle 5 --> row 1, col 0
@@ -135,8 +153,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 5 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -150,8 +168,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 6 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -165,8 +183,8 @@ namespace eudaq {
             i=ix*plane.m_ysize+iy; //pixel id in for m_pix
             j=(gate*plane.m_ysize/2) + (col*4) + 7 ;   //pixel id in our data
 
-            plane.m_x[i] =ix;
-            plane.m_y[i] =iy;
+            plane.m_x[0][i] =ix;
+            plane.m_y[0][i] =iy;
             plane.m_pix[0][i] = getlittleendian<unsigned short>(&data[12 + 2*j]);
 
 
@@ -180,14 +198,43 @@ namespace eudaq {
         unsigned npixels = plane.m_xsize * plane.m_ysize;
         for (size_t i = 0; i < npixels; ++i) {
           unsigned d = getlittleendian<unsigned>(&data[(3 + i)*4]);
-          plane.m_x[i] = (d >> 16) & 0x3f;
-          plane.m_y[i] = (d >> 22) & 0x7f;
+          plane.m_x[0][i] = (d >> 16) & 0x3f;
+          plane.m_y[0][i] = (d >> 22) & 0x7f;
           plane.m_pix[0][i] = d & 0xffff;
         }
       }
       return plane;
     }
-  };
+
+ protected:
+    static size_t NumPlanes(const Event & event) {
+      if (const RawDataEvent * ev = dynamic_cast<const RawDataEvent *>(&event)) {
+        return ev->NumBlocks();
+      } else if (const DEPFETEvent * ev = dynamic_cast<const DEPFETEvent *>(&event)) {
+        return ev->NumBoards();
+      }
+      return 0;
+    }
+
+    static std::vector<unsigned char> GetPlane(const Event & event, size_t i) {
+      if (const RawDataEvent * ev = dynamic_cast<const RawDataEvent *>(&event)) {
+        return ev->GetBlock(i);
+      } else if (const DEPFETEvent * ev = dynamic_cast<const DEPFETEvent *>(&event)) {
+        return ev->GetBoard(i).GetDataVector();
+      }
+      return std::vector<unsigned char>();
+    }
+
+    static size_t GetID(const Event & event, size_t i) {
+      if (const RawDataEvent * ev = dynamic_cast<const RawDataEvent *>(&event)) {
+        return ev->GetID(i);
+      } else if (const DEPFETEvent * ev = dynamic_cast<const DEPFETEvent *>(&event)) {
+        return ev->GetBoard(i).GetID();
+      }
+      return 0;
+    }
+
+  }; // end Base
 
   /********************************************/
 
@@ -196,6 +243,16 @@ namespace eudaq {
     //virtual lcio::LCEvent * GetLCIOEvent( eudaq::Event const * ee ) const;
 
     virtual bool GetStandardSubEvent(StandardEvent &, const eudaq::Event &) const;
+
+#if USE_LCIO && USE_EUTELESCOPE
+    virtual void GetLCIORunHeader(lcio::LCRunHeader & header, eudaq::Event const & bore, eudaq::Configuration const & conf) const {
+      return ConvertLCIOHeader(header, bore, conf);
+    }
+
+    virtual bool GetLCIOSubEvent(lcio::LCEvent & lcioEvent, const Event & eudaqEvent) const {
+      return ConvertLCIO(lcioEvent, eudaqEvent);
+    }
+#endif
 
   private:
     DEPFETConverterPlugin() : DataConverterPlugin("DEPFET") {}
@@ -221,6 +278,131 @@ namespace eudaq {
     }
     return true;
   }
+
+#if USE_LCIO && USE_EUTELESCOPE
+  void DEPFETConverterBase::ConvertLCIOHeader(lcio::LCRunHeader & header, eudaq::Event const & bore, eudaq::Configuration const & /*conf*/) const {
+    eutelescope::EUTelRunHeaderImpl runHeader(&header);
+//    runHeader.setDAQHWName(EUTELESCOPE::DEPFET);
+    //   printf("DEPFETConverterBase::ConvertLCIOHeader \n");
+
+    unsigned numplanes = bore.GetTag("BOARDS", 0);
+    runHeader.setNoOfDetector(numplanes);
+    std::vector<int> xMin(numplanes, 0), xMax(numplanes, 63), yMin(numplanes, 0), yMax(numplanes, 255);
+    for (unsigned i = 0; i < numplanes; ++i) {
+      const int id = bore.GetTag("ID" + to_string(i), i);
+//      const BoardInfo & info = GetInfo(id);
+      xMax[i] = 64;
+      yMax[i] = 255;
+    }
+    runHeader.setMinX(xMin);
+    runHeader.setMaxX(xMax);
+    runHeader.setMinY(yMin);
+    runHeader.setMaxY(yMax);
+  }
+
+ bool DEPFETConverterBase::ConvertLCIO(lcio::LCEvent & result, const Event & source) const {
+    TrackerRawDataImpl *rawMatrix;
+
+    if (source.IsBORE()) {
+      // shouldn't happen
+      return true;
+    } else if (source.IsEORE()) {
+      // nothing to do
+      return true;
+    }
+    // If we get here it must be a data event
+
+    // prepare the collections for the rawdata and the zs ones
+    std::auto_ptr< lcio::LCCollectionVec > rawDataCollection ( new lcio::LCCollectionVec (lcio::LCIO::TRACKERRAWDATA) ) ;
+
+    // set the proper cell encoder
+    CellIDEncoder< TrackerRawDataImpl > rawDataEncoder ( eutelescope::EUTELESCOPE::MATRIXDEFAULTENCODING, rawDataCollection.get() );
+      
+
+    // a description of the setup
+    std::vector< eutelescope::EUTelSetupDescription * >  setupDescription;
+    size_t numplanes = NumPlanes(source);
+    for (size_t iPlane = 0; iPlane < numplanes; ++iPlane) {
+
+      StandardPlane plane = ConvertPlane(GetPlane(source, iPlane), GetID(source, iPlane));
+      //     printf("DEPFETConverterBase::ConvertLCIO numplanes=%d  \n", numplanes);
+        // The current detector is ...
+      eutelescope::EUTelPixelDetector * currentDetector = 0x0;
+      //   printf("DEPFETConverterBase::ConvertLCIO  1 %d  \n", plane.m_sensor);
+      //    if ( plane.m_sensor == "DEPFET" ) {
+      std::string  mode = "RAW2";
+   
+     currentDetector = new eutelescope::EUTelDEPFETDetector;
+    rawMatrix = new TrackerRawDataImpl;
+   
+      currentDetector->setMode( mode );
+      // storage of RAW data is done here according to the mode
+      //printf("XMin =% d, XMax=%d, YMin=%d YMax=%d \n",currentDetector->getXMin(),currentDetector->getXMax(),currentDetector->getYMin(),currentDetector->getYMax());
+      rawDataEncoder["xMin"]     = currentDetector->getXMin();
+      rawDataEncoder["xMax"]     = currentDetector->getXMax();
+      rawDataEncoder["yMin"]     = currentDetector->getYMin();
+      rawDataEncoder["yMax"]     = currentDetector->getYMax();
+      rawDataEncoder["sensorID"] = 6;
+      rawDataEncoder.setCellID (rawMatrix);
+
+ 
+      //     std::vector<short> adcVec = plane.GetPixels<short>();
+
+       
+    size_t nPixel = plane.m_x.size();
+    // printf(" plane.m_x.size()=%d \n",plane.m_x.size());
+	for ( size_t iPixel = 0; iPixel < nPixel; ++iPixel ) {
+	    //printf("QQQ 0  %d %d \n",iPixel,(size_t)plane.m_pix[0][ iPixel ]);
+	    rawMatrix->adcValues().push_back( (size_t)plane.m_pix[0][ iPixel ] ) ;
+	}
+       rawDataCollection->push_back(rawMatrix);
+    
+       if ( result.getEventNumber() == 0 ) {
+           setupDescription.push_back( new eutelescope::EUTelSetupDescription( currentDetector )) ;
+       }
+ 
+       // add the collections to the event only if not empty!
+       if ( rawDataCollection->size() != 0 ) {
+	   result.addCollection( rawDataCollection.release(), "rawdata_dep" );
+       }
+       // this is the right place to prepare the TrackerRawData
+        // object
+
+    }
+
+ 
+    if ( result.getEventNumber() == 0 ) {
+
+      // do this only in the first event
+
+      LCCollectionVec * depfetSetupCollection = NULL;
+      bool depfetSetupExists = false;
+      try { 
+        depfetSetupCollection = static_cast< LCCollectionVec* > ( result.getCollection( "depfetSetup" ) ) ;
+        depfetSetupExists = true;
+      } catch (...) {
+        depfetSetupCollection = new LCCollectionVec( lcio::LCIO::LCGENERICOBJECT );
+      }
+
+      for ( size_t iPlane = 0 ; iPlane < setupDescription.size() ; ++iPlane ) {
+ 
+       depfetSetupCollection->push_back( setupDescription.at( iPlane ) );
+ 
+     }
+
+      if (!depfetSetupExists) {
+ 
+       result.addCollection( depfetSetupCollection, "depfetSetup" );
+ 
+      }
+    }
+
+
+    //     printf("DEPFETConverterBase::ConvertLCIO return true \n");
+   return true;
+ }
+
+ #endif
 
   /********************************************/
 
