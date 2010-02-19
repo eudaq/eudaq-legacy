cfa30b4d327a75352b0f9363ff5c2c5770c2ec50
killenb <killenb>
2010-02-19 13:39:46 +0000

    made TimePixBore streamable

---
+++ main/include/eudaq/TimePixBore.hh
@@ -0,0 +1,80 @@
+#ifndef EUDAQ_TIMEPIX_BORE_H
+#define EUDAQ_TIMEPIX_BORE_H
+
+// the svn revision of the corresponding tpccondata class
+// unfirtunatelz this has to be updated manually :-(
+#define SVN_REVISION 1853
+
+#include "eudaq/Event.hh"
+#include <string>
+
+// The common.h comes from pixelman
+// Is is only needed for the special pixelman constructor
+#ifdef PIXELMAN_FOR_EUDAQ
+  #include "common.h"
+#endif
+
+namespace eudaq {
+
+/**
+ *  At the begin of a run the Pixelman writes sends the timepix config in a BORE.
+ *  As the size of the data structures varies in size, depeding on the type of chip
+ *  and the number of chips, it does not exist as one memory block.
+ *  This class provides a collection of the required variables as well as a 
+ *  serialiser and deserialiser.
+ *
+ *  The constructor which gets the Pixelman specific data structures is only 
+ *  compiled in if the PIXELMAN_FOR_EUDAQ preprocessor flag is set. It is not needed
+ *  to deserialise the class and thus the main Eudaq does not have a Pixelman dependency.
+ */
+class TimePixBore: public Event
+{
+ public:
+        // The constructor used by the pixelman producer.
+        // Only available from within pixelman
+
+#ifdef PIXELMAN_FOR_EUDAQ
+  explicit TimePixBore( unsigned int run, DevInfo *param, double timeToEndOfShutter, 
+			double shutterLength, int moduleID,
+			DACTYPE *dacVals, size_t sizeOfDacVals);
+#endif
+
+	/// the constructor used by the data collector. It gets a data stream from the deserializer
+	explicit TimePixBore(Deserializer &);
+
+	/// due to the inhomogeneous data structure this thing needs it's own serialise function
+	virtual void Serialize (Serializer &) const;
+	
+	unsigned int GetRevision(){ return m_revision; }
+	int GetNChips(){ return m_rowLen * m_numberOfRows; }
+	int GetMPXType(){ return  m_mpxType; }
+	std::string GetChipBoardIDString(){ return m_chipboardIDstring; }
+	int GetModuleID(){ return m_moduleID; }
+	std::string GetInterfaceName(){ return m_ifaceName; }
+	double GetClock(){ return m_clockTimepix; }
+	double GetTimeToEndOfShutter(){ return m_timeToEndOfShutter; }
+	double GetShutterLength(){ return  m_shutterLength; }
+	std::vector<unsigned short> GetDACValues(){ return m_dacVals; }
+
+
+ protected:
+
+	unsigned int m_revision; ///< the MarlinTPC svn revision of the correconding 
+	                         ///< TPCCondData class
+	int m_rowLen;            ///< number of chips per row
+	int m_numberOfRows;      ///< number of rows in which chips are aligned
+	                         ///< (e.g. quad has 4 chips, which are in 2 rows)
+	int m_mpxType;           ///< medipix type - MPX_ORIG, MPX_MXR, MPX_TPX
+	std::string m_chipboardIDstring; ///< id string of chip/chipboard
+	std::string m_ifaceName;      ///< name oof the readout interface
+	double m_clockTimepix;        ///< the timepix clock
+	double m_timeToEndOfShutter;  ///< time from trigger to end of shutter
+	double m_shutterLength; ///< length of the shutter window
+	int m_moduleID;          ///< the module ID (LCIO CellID1)
+	std::vector<unsigned short> m_dacVals; ///< the dac setting of all chips
+
+};
+
+} // namespace eudaq
+
+#endif // EUDAQ_TIMEPIX_BORE_H
---
+++ main/src/TimePixBore.cc
@@ -0,0 +1,62 @@
+#include "eudaq/TimePixBore.hh"
+
+
+namespace eudaq {
+
+#ifdef PIXELMAN_FOR_EUDAQ
+  TimePixBore::TimePixBore( unsigned int run, DevInfo *param, double timeToEndOfShutter,
+			    double shutterLength, int moduleID,
+			    DACTYPE *dacVals, size_t sizeOfDacVals)
+    : Event(run, 0,     NOTIMESTAMP, Event::FLAG_BORE), // this is always a BORE, so event 0, no timestamp
+      //           run, event, timestamp,   flags
+      m_revision( SVN_REVISION ),  m_rowLen(param->rowLen),  m_numberOfRows( param->numberOfRows),
+      m_mpxType( param->mpxType ),  m_chipboardIDstring( param->chipboardID, MPX_MAX_CHBID),
+      m_ifaceName( param->ifaceName ), m_clockTimepix( param->clockTimepix),
+      m_timeToEndOfShutter( timeToEndOfShutter ), m_shutterLength( shutterLength ),
+      m_moduleID( moduleID ), m_dacVals( sizeOfDacVals )
+  {
+    for ( size_t index = 0 ; index < sizeOfDacVals ; ++index )
+    {
+      m_dacVals[index] = dacVals[index];
+    }
+  }
+#endif
+
+   TimePixBore::TimePixBore(Deserializer & ds )
+     : Event( ds )
+   {
+     ds.read( m_revision );
+     ds.read( m_rowLen );
+     ds.read( m_numberOfRows );
+     ds.read( m_mpxType );
+     ds.read( m_chipboardIDstring );
+     ds.read( m_ifaceName );
+     ds.read( m_clockTimepix );
+     ds.read( m_timeToEndOfShutter );
+     ds.read( m_shutterLength );
+     ds.read( m_moduleID );
+     ds.read( m_dacVals );
+   }
+
+   void TimePixBore::Serialize (Serializer &s) const
+   {
+     // First call the serialiser of the event
+     // In the deserialiser constructor the construcor of the mother class (Event) 
+     // is called before the constructor of bore is called
+     Event::Serialize(s);
+
+     // now serialise all member variables
+    s.write( m_revision );
+    s.write( m_rowLen );
+    s.write( m_numberOfRows );
+    s.write( m_mpxType );
+    s.write( m_chipboardIDstring );
+    s.write( m_ifaceName );
+    s.write( m_clockTimepix );
+    s.write( m_timeToEndOfShutter );
+    s.write( m_shutterLength );
+    s.write( m_moduleID );
+    s.write( m_dacVals );
+   }
+
+} // namespace eudaq
