9f4a1002d04e4bed1dda4ebff8cd59c98d63b214
Richard Peschke <richard.peschke@desy.de>
2013-07-26 17:28:33 +0000

    start scripts

---
+++ Windows Binaries/startGUIEudaq.bat
@@ -0,0 +1,9 @@
+start euRun.exe
+timeout /T 1  > nul
+start euLog.exe
+timeout /T 1  > nul
+start TestDataCollector.exe
+timeout /T 1  > nul
+start ni_Producer.exe
+timeout /T 1  > nul
+start TLUProducer.exe
